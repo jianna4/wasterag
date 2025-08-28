ccc5b8d77550089736181f4791340589e864729f
Joan Maina <mainajoan555@gmail.com>
2025-08-28 08:50:15 +0300

    for wastewise

---
+++ waster/pdfchunks.py
@@ -0,0 +1,22 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+import json
+loader.pyPDFloader(r"F:\projects\wasteRAG\fol\MUT-all-Programmes-2.pdf")
+loader.raise_for_errors = True
+docs = loader.load()
+print(f"Loaded {len(docs)} documents from PDF.")
+
+
+
+#splitting the document
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+documents = splitter.split_documents(docs)
+
+#saving as ajson file
+json_data = [
+    {"content": doc.page_content, "metadata": doc.metadata}
+    for doc in documents
+]
+with open("chunks.json", "w", encoding="utf-8") as f:
+    json.dump(json_data, f, indent=4, ensure_ascii=False)
